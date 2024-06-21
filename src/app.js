f0b1d56afa067605f1d7cf895a85557da9bf45b8
AngelCS94 <163451592+AngelCS94@users.noreply.github.com>
2024-06-21 18:02:06 +0000

    [update: app.js] - app.js : Comentarios en exceso.

--- src/app.js
+++ src/app.js
@@ -21,31 +21,31 @@ let matrizArrays = [who, action, what, when];
 const button = document.querySelector("button");
 button.addEventListener("click", onButtonClick);
 
-// Llamando a la función de generar excusas una vez cargue la ventana
+
 window.onload = function() {
   generadorDeExcusas();
 };
 
-// Función que genera excusas
+
 function generadorDeExcusas() {
   // Importante vaciarlo para que se pueda renovar la excusa al clicar en el botón
   finalArray = [];
 
-  // Aquí es la parte de la funcion que va eligiendo distintas posiciones para cada array de la matriz
+  
   matrizArrays.forEach(array => {
     finalArray.push(array[Math.floor(Math.random() * array.length)]);
   });
 
   // Aquí mandando al html que de base el div de "excusa" que dirá "Click the button to start!"
-  document.getElementById("excuse").innerHTML =
-    "<p> Click the button to start! </p>";
+  document.getElementById("excuse").innerHTML = 
+    "<p> Click the button to start! </p>"; 
 }
 
-// Función para que funcione el botón y al clicar genere una nueva excusa
+
 function onButtonClick() {
-  // Llamo a la función que generará la excusa antes de enviar nada
+  
   generadorDeExcusas();
-  // Lo mando a Html en forma de párrafo para luego poder modificar en style el "p"
+  
   document.getElementById("excuse").innerHTML =
     "<p>" + finalArray.join(" ") + "</p>";
 }
